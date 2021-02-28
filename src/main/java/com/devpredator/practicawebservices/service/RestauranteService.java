f886789191610ee99d12c255ca393e3d169573b9
dragokl89 <c-adonis@hotmail.com>
2021-02-27 21:58:19 -0500

    final

--- src/main/java/com/devpredator/practicawebservices/service/RestauranteService.java
+++ src/main/java/com/devpredator/practicawebservices/service/RestauranteService.java
@@ -3,10 +3,34 @@
  */
 package com.devpredator.practicawebservices.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.devpredator.practicawebservices.entity.Restaurante;
+
 /**
  * @author 4PF28LA_2004
  *
  */
 public class RestauranteService {
-
+ public List<Restaurante> consultarRestaurantes(){
+	 List<Restaurante> restaurantes = new ArrayList();
+	 
+	 Restaurante res = new Restaurante();
+	 res.setNombre("KFC");
+	 res.setIdRestaurante(123L);
+	 res.setSlogan("Hasta chuparse los dedos");
+	 res.setDireccion("Amazonas");
+	 
+	 Restaurante rest = new Restaurante();
+	 rest.setNombre("McDonald");
+	 rest.setIdRestaurante(456L);
+	 rest.setSlogan("No se la verdad");
+	 rest.setDireccion("Colon");
+	 
+	 restaurantes.add(rest);
+	 restaurantes.add(res);
+	 
+	 return restaurantes;
+ }
 }
--- src/main/java/com/devpredator/practicawebservices/ws/RestaurantesWS.java
+++ src/main/java/com/devpredator/practicawebservices/ws/RestaurantesWS.java
@@ -3,10 +3,67 @@
  */
 package com.devpredator.practicawebservices.ws;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.devpredator.practicawebservices.entity.Restaurante;
+import com.devpredator.practicawebservices.service.RestauranteService;
+
 /**
  * @author 4PF28LA_2004
  *
  */
+
+@Path("restaurantesWS")
 public class RestaurantesWS {
+
+	private RestauranteService restauranteService = new RestauranteService();
+
+	
+
+	@GET
+	@Path("test")
+	public String test() {
+		return "Probando funcionamiento";
+	}
+	
+	@GET
+	@Path("consultarRestaurantes")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public List<Restaurante> consultarRestaurantes() {
+		return this.restauranteService.consultarRestaurantes();
+	}
+
+	@POST
+	@Path("guardarRestaurante")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response guardarRestaurante(Restaurante restaurante) {
+		if (restaurante == null) {
+			return Response.status(400).entity("no se ingreso informacion del restaurante ,porfavor capturar los datos")
+					.build();
+		}
+
+		if (restaurante.getIdRestaurante() == null || restaurante.getIdRestaurante() == 0) {
+			return Response.status(400).entity("El id es requerido").build();
+		}
+
+		if (restaurante.getNombre() == null || restaurante.getNombre().isEmpty()) {
+			return Response.status(400).entity("El nombre es requerido").build();
+		}
+
+		GenericEntity<Restaurante> restauranteGeneric = new GenericEntity<Restaurante>(restaurante, Restaurante.class);
+		return Response.ok(restauranteGeneric).build();
+
+	}
 
 }
